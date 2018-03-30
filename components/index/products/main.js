ecdf27d8bf3a10e55e4d6764b85e919f845f0f60
jsdcxy <37364799+jsdcxy@users.noreply.github.com>
2018-03-30 15:55:22 +0800

    调整布局，增加数据

--- components/index/products/main.js
+++ components/index/products/main.js
@@ -1,7 +1,22 @@
+import Vue from 'vue'
 export default {
     data() {
       return {
+        images: [],
         currentDate: new Date()
       };
+    },
+    created() {
+      this.getImages()
+    },
+    methods: {
+      getImages() {
+          Vue.$http.get('/shiwu').then(res => {
+              this.images =res.data
+              //console.log(res)
+          }).catch(function (error) {
+              console.log(error);
+            });
+      }
     }
   }
